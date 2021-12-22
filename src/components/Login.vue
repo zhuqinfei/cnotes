6118f82e5d1ce975b967d3ec8660068b7d2699af
zhuqinfei <1586859762@qq.com>
2021-12-22 16:59:54 +0800

    配置基本组件

---
+++ src/components/Login.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="login">
+    <h1>{{msg}}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      msg: 'This is login page'
+    }
+  }
+}
+</script>
+
+<style scoped>
+h1 {
+  color: red;
+}
+</style>
---
+++ src/components/NoteDetail.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="note-detail">
+    <h1>{{msg}} : {{ $route.params.noteId }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      msg: '笔记详情页'
+    }
+  }
+}
+</script>
+
+<style scoped>
+h1 {
+  color: blue;
+}
+</style>
