b3703448421d2f8a49ab51359766d85562b2a6e7
zhuqinfei <1586859762@qq.com>
2021-12-24 00:45:57 +0800

    体验优化&动画

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -6,19 +6,23 @@
           <div class="main"></div>
           <div class="form">
             <h3 @click="showRegister">创建账户</h3>
-            <div v-show="isShowRegister" class="register">
-              <input type="text" v-model="register.username" placeholder="用户名" />
-              <input type="password" v-model="register.password" placeholder="密码" />
-              <p v-bind:class="{ error: register.isError }">{{ register.notice }}</p>
-              <div class="button" @click="onRegister">创建账号</div>
-            </div>
+            <transition name="slide">
+              <div v-bind:class="{show:isShowRegister}" class="register">
+                <input type="text" v-model="register.username" placeholder="用户名" />
+                <input type="password" v-model="register.password" placeholder="密码" />
+                <p v-bind:class="{ error: register.isError }">{{ register.notice }}</p>
+                <div class="button" @click="onRegister">创建账号</div>
+              </div>
+            </transition>
             <h3 @click="showLogin">登录</h3>
-            <div v-show="isShowLogin" class="login">
-              <input type="text" v-model="login.username" placeholder="输入用户名" />
-              <input type="password" v-model="login.password" placeholder="密码" />
-              <p v-bind:class="{ error: login.isError }">{{ login.notice }}</p>
-              <div class="button" @click="onLogin">登录</div>
-            </div>
+            <transition name="slide">
+              <div v-bind:class="{show:isShowLogin}" class="login">
+                <input type="text" v-model="login.username" placeholder="输入用户名" />
+                <input type="password" v-model="login.password" placeholder="密码" />
+                <p v-bind:class="{ error: login.isError }">{{ login.notice }}</p>
+                <div class="button" @click="onLogin">登录</div>
+              </div>
+            </transition>
           </div>
         </div>
       </div>
@@ -76,7 +80,7 @@ export default {
       console.log('开始注册，用户名是:', this.register.username, '密码是:', this.register.password)
     },
     onLogin() {
-       let result = this.validUsername(this.login.username)
+      let result = this.validUsername(this.login.username)
       if (!result.isValid) {
         this.login.isError = true
         this.login.notice = result.notice
@@ -148,8 +152,10 @@ export default {
   .form {
     width: 270px;
     border-left: 1px solid #ccc;
+    overflow: hidden;
     h3 {
       padding: 10px 20px;
+      margin-top: -1px;
       font-weight: normal;
       font-size: 16px;
       border-top: 1px solid #eee;
@@ -172,8 +178,14 @@ export default {
     }
     .login,
     .register {
-      padding: 10px 20px;
+      padding: 0px 20px;
       border-top: 1px solid #eee;
+      height: 0;
+      overflow: hidden;
+      transition: height .4s;
+      &.show {
+        height: 193px;
+      }
       input {
         display: block;
         width: 100%;
