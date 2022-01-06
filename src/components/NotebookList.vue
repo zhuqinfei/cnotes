a7d61cbb3a8e24d68550157a06df62cebaa292d3
zhuqinfei <1586859762@qq.com>
2022-01-06 22:46:42 +0800

    笔记本列表静态页面

--- src/components/NotebookList.vue
+++ src/components/NotebookList.vue
@@ -1,10 +1,35 @@
 <template>
-  <div id="notebook-list">
-    <h1>{{msg}}</h1>
-    <ul>
-      <li><router-link to="/note/1">笔记本1</router-link></li>
-      <li><router-link to="/note/2">笔记本2</router-link></li>
-    </ul>
+  <div id="notebook-list" class="detail">
+    <header>
+      <a href="#" class="btn">
+        <i class="iconfont icon-plus"></i>新建笔记本
+      </a>
+    </header>
+    <main>
+      <div class="layout">
+        <h3>笔记本列表(10)</h3>
+        <div class="book-list">
+          <a href="#" class="notebook">
+            <div>
+              <span class="iconfont icon-notebook"></span>笔记本标题1
+              <span>3</span>
+              <span class="action">编辑</span>
+              <span class="action">删除</span>
+              <span class="date">3天前</span>
+            </div>
+          </a>
+          <a href="#" class="notebook">
+            <div>
+              <span class="iconfont icon-notebook"></span>笔记本标题2
+              <span>1</span>
+              <span class="action">编辑</span>
+              <span class="action">删除</span>
+              <span class="date">5天前</span>
+            </div>
+          </a>
+        </div>
+      </div>
+    </main>
   </div>
 </template>
 
@@ -12,24 +37,22 @@
 import Auth from '../apis/auth'
 
 export default {
-  data () {
+  data() {
     return {
       msg: '笔记本列表'
     }
   },
-  created(){
+  created() {
     Auth.getInfo()
-      .then(res=>{
-        if(!res.isLogin){
-          this.$router.push({path:'/login'})
+      .then(res => {
+        if (!res.isLogin) {
+          this.$router.push({ path: '/login' })
         }
       })
   }
 }
 </script>
 
-<style scoped>
-h1 {
-  color: red;
-}
+<style lang="less" scoped>
+@import url(../assets/css/notebook-list.less);
 </style>
