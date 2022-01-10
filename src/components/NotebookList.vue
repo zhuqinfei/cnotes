d1592863a914d2b4f973d5880cb2ff18ead86bc9
zhuqinfei <1586859762@qq.com>
2022-01-10 23:43:06 +0800

    完成编辑、删除、新建等交互功能

--- src/components/NotebookList.vue
+++ src/components/NotebookList.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="notebook-list" class="detail">
     <header>
-      <a href="#" class="btn">
+      <a href="#" class="btn" @click.prevent="onCreate">
         <i class="iconfont icon-plus"></i>新建笔记本
       </a>
     </header>
@@ -9,12 +9,19 @@
       <div class="layout">
         <h3>笔记本列表(10)</h3>
         <div class="book-list">
-          <router-link v-for="(notebook,index) in notebooks" :key="index" to="/note/1" href="#" class="notebook">
+          <router-link
+            v-for="(notebook,index) in notebooks"
+            :key="index"
+            to="/note/1"
+            href="#"
+            class="notebook"
+          >
             <div>
-              <span class="iconfont icon-notebook"></span>{{notebook.title}}
-              <span>{{notebook.noteCounts}}</span>
-              <span class="action">编辑</span>
-              <span class="action">删除</span>
+              <span class="iconfont icon-notebook"></span>
+              {{ notebook.title }}
+              <span>{{ notebook.noteCounts }}</span>
+              <span class="action" @click.stop.prevent="onEdit(notebook)">编辑</span>
+              <span class="action" @click.stop.prevent="onDelete(notebook)">删除</span>
               <span class="date">3天前</span>
             </div>
           </router-link>
@@ -26,14 +33,15 @@
 
 <script>
 import Auth from '../apis/auth'
+import notebooks from '../apis/notebooks'
 import Notebooks from '../apis/notebooks'
 
-window.Notebooks=Notebooks
+window.Notebooks = Notebooks
 
 export default {
   data() {
     return {
-       notebooks:[],
+      notebooks: [],
     }
   },
   created() {
@@ -43,10 +51,45 @@ export default {
           this.$router.push({ path: '/login' })
         }
       })
-      Notebooks.getAll()
-        .then(res=>{
-           this.notebooks=res.data
+    Notebooks.getAll()
+      .then(res => {
+        this.notebooks = res.data
+      })
+  },
+  methods: {
+    onCreate() {
+      let title = window.prompt('创建笔记本')
+      if (title.trim() === '') {
+        alert('笔记本名不能为空')
+        return
+      }
+      Notebooks.addNotebook({ title })
+        .then(res => {
+          console.log(res)
+          this.notebooks.unshift(res.data)
+          alert(res.msg)
         })
+    },
+    onEdit(notebook) {
+      let title = window.prompt('修改标题', notebook.title)
+      Notebooks.updateNotebooks(notebook.id, { title })
+        .then(res => {
+          console.log(res)
+          notebook.title = title
+          alert(res.msg)
+        })
+    },
+    onDelete(notebook) {
+      let isconfirm = window.confirm('你确实要删除吗？')
+      if (isconfirm) {
+        Notebooks.deleteNotebook(notebook.id)
+          .then(res => {
+            console.log(res)
+            this.notebooks.splice(this.notebooks.indexOf(notebook), 1)
+            alert(res.msg)
+          })
+      }
+    }
   }
 }
 </script>
