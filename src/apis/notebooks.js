e8e53eab69451d60f48efcd7ed101fb9bb575f29
zhuqinfei <1586859762@qq.com>
2022-01-15 23:58:05 +0800

    完成NotebookList组件数据交互

--- src/apis/notebooks.js
+++ src/apis/notebooks.js
@@ -1,27 +1,39 @@
 import request from "../helpers/request";
+import { friendlyDate } from '../helpers/util'
 
-const URL={
-    GET:'/notebooks',
-    ADD:'/notebooks',
-    UPDATE:'/notebooks/:id',
-    DELETE:'/notebooks/:id'
+const URL = {
+    GET: '/notebooks',
+    ADD: '/notebooks',
+    UPDATE: '/notebooks/:id',
+    DELETE: '/notebooks/:id'
 }
 
-export default{
-    getAll(){
-        return request(URL.GET)
+export default {
+    getAll() {
+        return new Promise((resolve, reject) => {
+            request(URL.GET)
+                .then(res => {
+                    res.data = res.data.sort((notebook1, notebook2) => notebook1.createAt < notebook2.createAt ? 1 : -1)
+                    res.data.forEach(notebook => {
+                        notebook.friendlyCreatedAt = friendlyDate(notebook.createdAt)
+                    })
+                    resolve(res)
+                }).catch(err => {
+                    reject(err)
+                })
+        })
     },
 
-    updateNotebooks(notebookId,{title=''}={title:''}){
-        return request(URL.UPDATE.replace(':id',notebookId),'PATCH',{title})
+    updateNotebooks(notebookId, { title = '' } = { title: '' }) {
+        return request(URL.UPDATE.replace(':id', notebookId), 'PATCH', { title })
     },
 
-    deleteNotebook(notebookId){
-        return request(URL.DELETE.replace(':id',notebookId),'DELETE')
+    deleteNotebook(notebookId) {
+        return request(URL.DELETE.replace(':id', notebookId), 'DELETE')
     },
 
-    addNotebook({title=''}={title:''}){
-        return request(URL.ADD,'post',{title})
+    addNotebook({ title = '' } = { title: '' }) {
+        return request(URL.ADD, 'post', { title })
     }
 
 }
--- src/components/NotebookList.vue
+++ src/components/NotebookList.vue
@@ -22,7 +22,7 @@
               <span>{{ notebook.noteCounts }}</span>
               <span class="action" @click.stop.prevent="onEdit(notebook)">编辑</span>
               <span class="action" @click.stop.prevent="onDelete(notebook)">删除</span>
-              <span class="date">3天前</span>
+              <span class="date">{{notebook.friendlyCreatedAt}}</span>
             </div>
           </router-link>
         </div>
@@ -33,8 +33,8 @@
 
 <script>
 import Auth from '../apis/auth'
-import notebooks from '../apis/notebooks'
 import Notebooks from '../apis/notebooks'
+import { friendlyDate } from '@/helpers/util'
 
 window.Notebooks = Notebooks
 
@@ -66,6 +66,7 @@ export default {
       Notebooks.addNotebook({ title })
         .then(res => {
           console.log(res)
+          res.data.friendlyCreatedAt = friendlyDate(res.data.createdAt)
           this.notebooks.unshift(res.data)
           alert(res.msg)
         })
