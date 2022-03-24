e442b06ca4bf25a9530725698a3ca4c31dc48e45
zhuqinfei <1586859762@qq.com>
2022-03-25 00:14:53 +0800

    笔记详情列表的结构和样式

--- src/components/NoteDetail.vue
+++ src/components/NoteDetail.vue
@@ -1,33 +1,42 @@
 <template>
-  <div id="note-detail">
-    <!-- /note?noteId=1&notebookId=2  获取这样参数的用$route.query-->
-    <h1>notebookId:{{$route.query.notebookId}}</h1>   
-    <h1>noteId:{{$route.query.noteId}}</h1>
+  <div id="note" class="detail">
+    <note-sidebar></note-sidebar>
+    <div id="note-detail">
+      <!-- /note?noteId=1&notebookId=2  获取这样参数的用$route.query-->
+      <h1>notebookId:{{ $route.query.notebookId }}</h1>
+      <h1>noteId:{{ $route.query.noteId }}</h1>
+    </div>
   </div>
 </template>
 
 <script>
-import Auth from '../apis/auth'
+import Auth from "../apis/auth";
+import NoteSidebar from "@/components/NoteSidebar";
 
 export default {
-  data () {
-    return {
-      msg: '笔记详情页'
-    }
+  components: {
+    NoteSidebar,
   },
-   created(){
-    Auth.getInfo()
-      .then(res=>{
-        if(!res.isLogin){
-          this.$router.push({path:'/login'})
-        }
-      })
-  }
-}
+  data() {
+    return {
+      msg: "笔记详情页",
+    };
+  },
+  created() {
+    Auth.getInfo().then((res) => {
+      if (!res.isLogin) {
+        this.$router.push({ path: "/login" });
+      }
+    });
+  },
+};
 </script>
 
 <style scoped>
-h1 {
-  color: blue;
+#note {
+  display: flex;
+  align-items: stretch;
+  background-color: #fff;
+  flex: 1;
 }
 </style>
---
+++ src/components/NoteSidebar.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="note-sidebar">
+    <span class="btn add-note" >添加笔记</span>
+    <el-dropdown class="notebook-title"  @command="handleCommand" placement="bottom">
+      <span class="el-dropdown-link">
+        我的笔记本1 <i class="iconfont icon-down"></i>
+      </span>
+      <el-dropdown-menu slot="dropdown">
+        <el-dropdown-item v-for="notebook in notebooks" :command="notebook.id" :key="notebook">{{notebook.title}}</el-dropdown-item>
+        <el-dropdown-item  command="trash">回收站</el-dropdown-item>
+      </el-dropdown-menu>
+    </el-dropdown>
+    <div class="menu">
+      <div>更新时间</div>
+      <div>标题</div>
+    </div>
+    <ul class="notes">
+      <li v-for="note in notes" :key="note">
+        <router-link :to="`/note?noteId=${note.id}`">
+          <span class="date">{{note.updatedAtFriendly}}</span>
+          <span class="title">{{note.title}}</span>          
+        </router-link>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+
+  export default {
+    data() {
+      return {
+        notebooks: [
+          {
+            id: 1,
+            title: 'hello1',
+          },
+          {
+            id: 2,
+            title: 'hello2',
+            updatedAtFriendly: '3分钟前'
+          }
+        ],
+        notes:[ 
+            {
+              id: 11,
+              title: '第1个笔记',
+              updatedAtFriendly: '刚刚'
+            },
+            {
+              id: 12,
+              title: '第2个笔记',
+              updatedAtFriendly: '3分钟前'
+            },
+          ]
+      }
+    },
+
+    methods: {
+      handleCommand(cmd) {
+        console.log(cmd)
+      }
+
+    }
+  }
+
+</script>
+
+
+<style lang="less" >
+@import url(../assets/css/note-sidebar.less);
+
+</style>
+
+
